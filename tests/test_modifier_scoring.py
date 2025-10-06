5ea59d11f008f358dd20f985b2198781bdb0debe
bbaasan <60349255+bb-github74@users.noreply.github.com>
2025-10-06 12:18:06 -0400

    Update test_modifier_scoring.py

--- tests/test_modifier_scoring.py
+++ tests/test_modifier_scoring.py
@@ -1,1 +1,7 @@
+from tnlp.modifier_scoring import compute_complexity
+
+def test_modifier_scoring():
+    sentence = "Although it might be profitable, we are not certain."
+    score = compute_complexity(sentence)
+    assert score > 0
 
